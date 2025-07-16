7d1e9d4e56e33d576684ae4dea80b6dc29673b89
ProjectMoosa <moosa.solution@gmail.com>
2025-07-17 00:46:16 +0100

    Revert lato-regular font changes for numbers in vendor management page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7bca8418d731bfc2")
+/******/ 		__webpack_require__.h = () => ("6403ac749976df69")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2cd1965176d1b78"; }
+/******/ 		__webpack_require__.h = function() { return "3548a5d2b67d9cec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
